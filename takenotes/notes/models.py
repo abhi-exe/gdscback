5a103a63064cb05a8fdbcfebe3d6994ad48f9de4
abhi-exe <evilcraft68@LAPTOP-3P961QFF.localdomain>
2022-05-31 16:43:44 +0530

    Another working model, uptil creating a login template without the actual login function

--- takenotes/notes/models.py
+++ takenotes/notes/models.py
@@ -1,3 +1,23 @@
+from email.policy import default
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
+
+class Note(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    save_date = models.DateTimeField(blank=True, null=True)
+    tags=models.CharField(max_length=200, null=True)
+
+    '''def addtag(self):
+        self.tags.append(models.CharField(max_length=40))'''
+
+    def savetoapp(self):
+        self.save_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
 # Create your models here.
