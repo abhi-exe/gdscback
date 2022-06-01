1de38bfd8f270503a27d54be6b3b2465086d40ed
abhi-exe <evilcraft68@LAPTOP-3P961QFF.localdomain>
2022-06-01 19:14:33 +0530

    Working on view,newnote, untill then the version is here

--- takenotes/notes/urls.py
+++ takenotes/notes/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='loginroute'),
+    path('mynotes',views.notelist, name='noteshomeroute'),
+    path('mynotes/<int:pk>/',views.viewnote, name='singlenote'),
+    path('createnote',views.newnote, name='newnote'),
 ]
--- takenotes/notes/views.py
+++ takenotes/notes/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Note
+from django.contrib.auth.models import User
 # Create your views here.
 
 def login(request):
+    #Make a login portal
+    user=User.objects.get(username="evilcraft68")
     return render(request,'notes/login.html',{})
+
+def notelist(request):
+    user=User.objects.get(username="evilcraft68")
+    notelist1=Note.objects.filter(author=user)
+    return render(request,'notes/noteslist.html',{'notelist': notelist1})
+
+def viewnote(request,pk):
+    note=get_object_or_404(Note, pk=pk)
+    return render(request, 'notes/viewnote.html',{'note':note})
+
+def newnote(request):
+    return
